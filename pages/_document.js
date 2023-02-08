18d561907219a91b277ab970818022137607fe7f
Mrityunjay Gupta <mrityunjaygupta1985@gmail.com>
2023-02-08 15:31:25 +0530

    https form sent

--- pages/_document.js
+++ pages/_document.js
@@ -37,8 +37,15 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8" />
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="mobile-web-app-capable" content="yes" />
+          <meta
+            httpEquiv="Content-Security-Policy"
+            content="upgrade-insecure-requests"
+          />
           <link rel="icon" href="/favicon.ico" />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
